46732db7d92daa49a1a2bf00cb2d95327336d725
jeyaprakash <jeyaprakash.media@gmail.com>
2020-06-13 15:15:46 +0530

    fragment life cycle - combined

--- sample1/app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ sample1/app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String message = "tag-1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(message,"Activity : onCreate");
         addFragment();
     }
 
@@ -21,5 +25,41 @@ public class MainActivity extends AppCompatActivity {
         SampleFragment sampleFragment = new SampleFragment();
         frgTransaction.add(R.id.frameContainer,sampleFragment);
         frgTransaction.commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(message,"Activity : onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(message,"Activity : onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(message,"Activity : onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(message,"Activity : onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(message,"Activity : onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(message,"Activity : onDestroy");
     }
 }
--- sample1/app/src/main/java/com/example/fragmenttest/SampleFragment.java
+++ sample1/app/src/main/java/com/example/fragmenttest/SampleFragment.java
@@ -1,9 +1,13 @@
 package com.example.fragmenttest;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +18,73 @@ import android.view.ViewGroup;
  */
 public class SampleFragment extends Fragment {
 
+    private static String message = "tag-2";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(message,"Fragment : onCreateView");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sample, container, false);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(message,"Fragment : onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(message,"Fragment : onCreate");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(message,"Fragment : onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(message,"Fragment : onStart");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(message,"Fragment : onPause");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(message,"Fragment : onPause");
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(message,"Fragment : onStop");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(message,"Fragment : onDetach");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(message,"Fragment : onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(message,"Fragment : onDestroy");
     }
 }
