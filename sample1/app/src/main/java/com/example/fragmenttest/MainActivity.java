1b504788108e3e4402323bd017644c0bca202311
jeyaprakash <jeyaprakash.media@gmail.com>
2020-06-13 16:41:15 +0530

    fragment addTobackstack with and without

--- sample1/app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ sample1/app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fragmenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -8,11 +9,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String message = "tag-1";
     Button btnAddFragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction frgTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,24 +24,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(message,"Activity : onCreate");
         btnAddFragment = (Button) findViewById(R.id.btnAddFragment);
+        final TextView txtFragmentCount = (TextView) findViewById(R.id.textviewCount);
+        fragmentManager = getSupportFragmentManager();
         btnAddFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addFragment();
             }
         });
+
+        txtFragmentCount.setText(" current back stack count is "+fragmentManager.getBackStackEntryCount());
+
+        fragmentManager.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                txtFragmentCount.setText(" current back stack count is "+fragmentManager.getBackStackEntryCount());
+            }
+        });
      }
 
     private void addFragment(){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction frgTransaction = fragmentManager.beginTransaction();
-        SampleFragment sampleFragment = new SampleFragment();
-        frgTransaction.add(R.id.frameContainer,sampleFragment);
+         /*  Fragment fragment;
+
+        switch (fragmentManager.getBackStackEntryCount()){
+            case 0:fragment = new SampleFragment();break;
+            case 1:fragment = new firstFragment();break;
+            case 2:fragment = new SecondFragment(); break;
+            case 3 :fragment = new ThirdFragment(); break;
+            default:fragment = new SampleFragment();break;
+        }
+         frgTransaction = fragmentManager.beginTransaction();
+        frgTransaction.add(R.id.frameContainer,fragment,"demofragment");
+        frgTransaction.addToBackStack(null);
         frgTransaction.commit();
+
+         */
+
+        Fragment fragment = fragmentManager.findFragmentById(R.id.frameContainer);
+
+        if(fragment instanceof SampleFragment){
+            fragment = new firstFragment();
+        }else if(fragment instanceof firstFragment){
+            fragment = new SecondFragment();
+        }else if(fragment instanceof SecondFragment){
+            fragment = new ThirdFragment();
+        }else if(fragment instanceof ThirdFragment){
+            fragment = new SampleFragment();
+        }else{
+            fragment=new SampleFragment();
+        }
+
+        frgTransaction = fragmentManager.beginTransaction();
+        frgTransaction.add(R.id.frameContainer,fragment);
+        frgTransaction.commit();
+
+
     }
 
+    @Override
+    public void onBackPressed() {
+        Fragment fragment = fragmentManager.findFragmentById(R.id.frameContainer);
 
-
+        if(fragment != null){
+            frgTransaction = fragmentManager.beginTransaction();
+            frgTransaction.remove(fragment);
+            frgTransaction.commit();
+        }else{
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onStart() {
